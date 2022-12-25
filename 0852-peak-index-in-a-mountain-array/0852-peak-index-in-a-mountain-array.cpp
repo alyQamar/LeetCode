a79fc827d3593ea483870975ea2345bde0253987
Aly Qamar <87208298+alyQamar@users.noreply.github.com>
2022-12-25 10:02:51 +0200

    Time: 132 ms (78.50%), Space: 59.7 MB (20.02%) - LeetHub

---
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int l=-0 , h= arr.size()-1 , m;
+        while(l < h){
+            m = l+(h-l)/2;
+            if(arr[m]<arr[m+1]){
+                l=m+1;
+            }else{
+                h=m;
+            }
+        }
+        return l;
+    }
+};
