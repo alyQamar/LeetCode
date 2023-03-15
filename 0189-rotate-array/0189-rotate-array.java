ba91af272cd91cfbff09a1b8b6be2160934f47ed
Aly Qamar <87208298+alyQamar@users.noreply.github.com>
2023-03-15 13:13:00 +0200

    Time: 1 ms (58.68%), Space: 58.6 MB (39.01%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -4,24 +4,27 @@ class Solution {
         int[] a = new int[k];
         k= k % nums.length;
         int len =nums.length;
-        boolean bol = (k==0||nums.length==1);
+      
+        //ASK About [See Constraints]
+        //1- Empty array
+        //2- if K may equal 0 ??
         
-        if(!bol){
         //handle first part 
         int cnt =0;
         for(int i=len-k;i<len;i++){
             a[cnt]= nums[i];
             cnt++;
         }
+        
         //shift array to leave space for the first part
         // must loop from last to first
         for(int i=len-k-1;i>=0;i--){
             nums[i+k]=nums[i];
         }
+        
         //put first part
         for(int i=0;i<k;i++){
             nums[i]=a[i];
         }    
-        }
     }
 }
