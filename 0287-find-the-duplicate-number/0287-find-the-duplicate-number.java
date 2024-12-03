7b38865eb1eb70470b48576a846225a2276ff8ff
Aly <87208298+alyQamar@users.noreply.github.com>
2024-12-03 03:00:32 +0200

    Time: 6 ms (51.83%), Space: 58.2 MB (46.77%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,12 +1,31 @@
 import java.util.Arrays;
 class Solution {
+// Approach #1: brute force
+// Time Complexity: O(n log n)
+// Space Complexity: : O(n) - Sorting requires additional space
+    // public int findDuplicate(int[] nums) {
+    //     Arrays.sort(nums);
+    //     for(int i =0;i<nums.length-1;i++){
+    //         if(nums[i]==nums[i+1]){
+    //             return nums[i];
+    //         }
+    //     }
+    //     return 0;
+    // }
+// Approach #2: BitSet
+// Time Complexity: O(n)
+// Space Complexity: :O(1)
+    private final int MAX_NUM = 100000;
     public int findDuplicate(int[] nums) {
-        Arrays.sort(nums);
-        for(int i =0;i<nums.length-1;i++){
-            if(nums[i]==nums[i+1]){
+        BitSet bits = new BitSet(MAX_NUM);
+        for(int i = 0; i<nums.length;i++){
+            if(bits.get( nums[i])){
                 return nums[i];
+            }else{
+                bits.set( nums[i]);
             }
         }
         return 0;
     }
+
 }
