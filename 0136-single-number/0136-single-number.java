5f6db81913bbaf2cf3e10589499a8f7caeb65bea
Aly <87208298+alyQamar@users.noreply.github.com>
2024-12-13 22:28:08 +0200

    Time: 1 ms (99.86%), Space: 46.1 MB (43.38%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,20 +1,40 @@
 class Solution {
-    public int singleNumber(int[] nums) {
-        Arrays.sort(nums);
-        if (nums.length == 1) {
-            return nums[0];
-        }
-        for(int i = 0; i<nums.length-1;i++){
-            if(nums[i]!=nums[i+1]){
-               return nums[i];
-            }else{
-                i++;
-                if(i == (nums.length-2)){
-                    return nums[i+1];
-                }
-            }
+    /*
+    * Approach 1: Sorting + check i, i+1
+    * Time Complexity: O(nlog(n))
+    * Space Complexity: O(1)
+    */
+//     public int singleNumber(int[] nums) {
+
+//         Arrays.sort(nums);
+//         if (nums.length == 1) {
+//             return nums[0];
+//         }
+//         for(int i = 0; i<nums.length-1;i++){
+//             if(nums[i]!=nums[i+1]){
+//                return nums[i];
+//             }else{
+//                 i++;
+//                 if(i == (nums.length-2)){
+//                     return nums[i+1];
+//                 }
+//             }
          
+//         }
+//         return 0;
+        
+//     }
+    /*
+    * Approach 1: Sorting + check i, i+1
+    * Time Complexity: O(n)
+    * Space Complexity: O(1)
+    */
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for(int i =0; i<nums.length;i++){
+            result ^= nums[i];
         }
-        return 0;
+        return result;
+        
     }
 }
